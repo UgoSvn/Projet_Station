1390417b6117ce6f1001381f156702cb2a86445c
Michael <michaelzogheib@gmail.com>
2020-03-25 21:05:44 +0100

    Fonction Km stationNèmePlusProche

--- src/fr/esigelec/jee/DomParser.java
+++ src/fr/esigelec/jee/DomParser.java
@@ -138,6 +138,47 @@ public class DomParser {
 		
 		
 	}
+	
+	public String stationKmLaNèmePlusProche(double latitude1, double longitude1, int n){
+		ArrayList<Double> ListeDouble = new ArrayList<Double>();
+		ArrayList<Double> ListeDouble2 = new ArrayList<Double>();
+		ArrayList<String> ListId = new ArrayList<String>();
+		DistanceCalculator A = new DistanceCalculator();
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		try {
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.parse("Stations.xml");
+			NodeList pdvList = doc.getElementsByTagName("pdv");
+			for(int i=0;i<pdvList.getLength();i++) {
+				Node p = pdvList.item(i);
+				if(p.getNodeType()==Node.ELEMENT_NODE) {
+					Element pdv = (Element) p;
+					String latitude = pdv.getAttribute("latitude");
+					String longitude = pdv.getAttribute("longitude");
+					String id = pdv.getAttribute("id");
+					if(latitude.length()==7&&longitude.length()==6) {
+					double latitude2 = stringToDoubleLatitude(latitude);
+					double longitude2 = stringToDoubleLongitude(longitude);
+					double d = A.distance(latitude1, longitude1, latitude2, longitude2);
+					ListeDouble.add(d);
+					
+					}
+				}
+			}
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) { 
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Collections.sort(ListeDouble);
+		return String.valueOf(ListeDouble.get(n-1));
+		
+	}
 
 	public static void main(String[] args) {
 /*		// TODO Auto-generated method stub
